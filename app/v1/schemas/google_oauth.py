bff03e776ae359fef1a1a5f873878166eee3355e
okunola <abdayoade@gmail.com>
2024-08-08 21:16:39 +0100

    feat: add google oauth schemas
    I added the several schemas needed for the google oauth login endpoint

---
+++ app/v1/schemas/google_oauth.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from pydantic import BaseModel, EmailStr
+
+class UserData(BaseModel):
+    """
+    Schema response for validated google login
+    """
+    id: str
+    username: str 
+    first_name: str
+    last_name: str 
+    email: EmailStr
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
+
+class Tokens(BaseModel):
+    """
+    Schema for generated tokens response
+    """
+    access_token: str
+    refresh_token: str
+    token_type: str
+
+class StatusResponse(BaseModel):
+    """
+    Response schema for the end user 
+    """
+    message: str
+    status: str
+    status_code: int = 200
+    tokens: Tokens
+    user: UserData
