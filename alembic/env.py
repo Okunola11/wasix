41c0bdb2d0503d41e9e4bed5ce6db6e3178987bb
okunola <abdayoade@gmail.com>
2024-08-10 08:41:44 +0100

    chore: corrected module name to app
    I corrected the import path for models from previous api to app.

--- alembic/env.py
+++ alembic/env.py
@@ -6,8 +6,8 @@ from sqlalchemy import pool
 from alembic import context
 
 from decouple import config as decouple_config
-from api.v1.models import *
-from api.db.database import Base
+from app.v1.models import *
+from app.db.database import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
