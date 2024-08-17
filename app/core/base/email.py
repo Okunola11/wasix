a7a1139a387647d611c6921732a70e6a6501ce3d
okunola <abdayoade@gmail.com>
2024-08-17 19:30:40 +0100

    refactor: changed BaseEmailSender intialization
    I change the BaseEmailSender class initialization to exclude user and
    backgroung_tasks attributes. Moved them as parameter to send_email()
    method of the class.

--- app/core/base/email.py
+++ app/core/base/email.py
@@ -5,17 +5,15 @@ from app.core.config.email import send_email
 from app.utils.settings import settings
 
 class BaseEmailSender:
-    def __init__(self, user: User, background_tasks: BackgroundTasks):
-        self.user = user
-        self.background_tasks = background_tasks
+    def __init__(self):
         self.app_name = settings.APP_NAME
         self.fronted_host = settings.FRONTEND_URL
 
-    async def send_email(self, subject, context, template_name):
+    async def send_email(self, user, subject, context, template_name, background_tasks: BackgroundTasks):
         await send_email(
-            recipients=[self.user.email],
+            recipients=[user.email],
             subject=subject,
             template_name=template_name,
             context=context,
-            background_tasks=self.background_tasks
+            background_tasks=background_tasks
         )
