24354b8d0f328aa44258eac05360825fbc04ff01
okunola <abdayoade@gmail.com>
2024-08-17 19:36:38 +0100

    chore: update email service
    I update email service syntax to match the change made to the
    BaseEmailSender class. I also intialized instances of each email
    service.

--- app/v1/services/email.py
+++ app/v1/services/email.py
@@ -1,38 +1,46 @@
+from fastapi import BackgroundTasks
+
+from app.v1.models.user import User
 from app.core.base.email import BaseEmailSender
+from app.core.config.security import hash_password
 from app.utils.email_context import USER_VERIFY_ACCOUNT, FORGOT_PASSWORD
 
 class SendAccountVerificationEmail(BaseEmailSender):
-    async def send(self):
-        string_context = self.user.get_context_string(USER_VERIFY_ACCOUNT)
-        token = string_context # it should be hashed
-        activate_url = f"{self.fronted_host}/auth/account-verify?token={token}&email={self.user.email}"
+    async def send(self, user: User, background_tasks: BackgroundTasks):
+        string_context = user.get_context_string(USER_VERIFY_ACCOUNT)
+        token = hash_password(string_context) # hashed token
+        activate_url = f"{self.fronted_host}/auth/account-verify?token={token}&email={user.email}"
         data = {
             'app_name': self.app_name,
-            'name': self.user.last_name,
+            'name': user.last_name,
             'activate_url': activate_url
         }
         subject = f"Account Verification - {self.app_name}"
-        await self.send_email(subject, data, "user/account-verification.html")
+        await self.send_email(user, subject, data, "user/account-verification.html", background_tasks)
 
 class SendAccountActivationConfirmationEmail(BaseEmailSender):
-    async def send(self):
+    async def send(self, user: User, background_tasks: BackgroundTasks):
         data = {
             'app_name': self.app_name,
-            'name': self.user.last_name,
+            'name': user.last_name,
             'login_url': self.fronted_host
         }
         subject = f"Welcome - {self.app_name}"
-        await self.send_email(subject, data, "user/account-verification-confirmation.html")
+        await self.send_email(user, subject, data, "user/account-verification-confirmation.html", background_tasks)
 
 class SendPasswordResetEmail(BaseEmailSender):
-    async def send(self):
+    async def send(self, user: User, background_tasks: BackgroundTasks):
         string_context = f"{self.user.get_context_string(FORGOT_PASSWORD)}"
-        token = string_context # hash this token
-        reset_url = f"{self.fronted_host}/reset-password?token={token}&email={self.user.email}"
+        token = hash_password(string_context) # hashed token
+        reset_url = f"{self.fronted_host}/reset-password?token={token}&email={user.email}"
         data = {
             'app_name': self.app_name,
-            'name': self.user.last_name,
+            'name': user.last_name,
             'activate_url': reset_url
         }
         subject = f"Reset Password - {self.app_name}"
-        await self.send_email(subject, data, "user/password-reset.html")
+        await self.send_email(user, subject, data, "user/password-reset.html", background_tasks)
+
+account_verification_email = SendAccountVerificationEmail()
+account_activation_confirmation_email = SendAccountActivationConfirmationEmail()
+password_reset_email = SendPasswordResetEmail()
