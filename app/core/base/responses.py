8b4dc9270017a95032efb14966d61ea687f0c896
okunola <abdayoade@gmail.com>
2024-08-20 17:53:55 +0100

    chore: update base response class
    I update the base response class to include the fields all responses use
    and added a base class for the data field.

--- app/core/base/responses.py
+++ app/core/base/responses.py
@@ -1,4 +1,14 @@
 from pydantic import BaseModel, ConfigDict
+from typing import Any, Optional
+
+class BaseResponseData(BaseModel):
+    """Base schema for response data"""
+
+    model_config = ConfigDict(from_attributes=True, arbitrary_types_allowed=True)
 
 class BaseResponse(BaseModel):
-    model_config = ConfigDict(from_attributes=True, arbitrary_types_allowed=True)
+    """Base schema for all responses"""
+
+    status_code: int = 200
+    message: str
+    data: Optional[Any] = {}
